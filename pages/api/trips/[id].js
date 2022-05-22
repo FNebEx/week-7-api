103a9f1f3c00fff8059e8a2af2544a43d97ad30a
Kevin Grant <anmoron11@gmail.com>
2022-05-22 12:21:56 -0400

    Add PUT and DELETE functionality

--- pages/api/trips/[id].js
+++ pages/api/trips/[id].js
@@ -2,21 +2,35 @@ import prisma from "lib/PrismaClient";
 
 export default async function handler(req, res) {
   if(req.method === 'GET') { 
+    console.log(typeof req.query.id);
     const trip = await prisma.trip.findUnique({
-      where: {
-        id: parseInt(req.query.id)
-      }
+      where: { id: parseInt(req.query.id) }
     });
 
-    if (!trip) {
-      return res.status(400).json({ messages: 'Not found' });
-    }
+    if (!trip) return res.status(400).json({ messages: 'Not found' });
 
     res.status(200).json(trip);
   }
 
-  if(req.method === 'PUT') { /* */ }
+  if(req.method === 'PUT') {
+    const { user, name, start_date, end_date } = req.body;
 
-  if(req.method === 'DELETE') { /* */ }
+    await prisma.trip.update({
+      data: { user, name, start_date, end_date },
+      where: { id: parseInt(req.query.id) }
+    });
+
+    return res.status(200).end();
+  }
+
+  if(req.method === 'DELETE') {
+    if(req.method === 'DELETE') {
+      await prisma.trip.delete({
+        where: { id: parseInt(req.query.id) }
+      });
+      
+      return res.status(200).end();
+    }
+  }
 
 }
